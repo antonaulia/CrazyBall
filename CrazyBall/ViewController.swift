5f3ffc4628a9fa1af1423ee773b7f1ec7e24d81c
antonaulia <antoniusferiantoaulia@gmail.com>
2020-06-09 14:19:29 +0700

    Initial Commit

---
+++ CrazyBall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CrazyBall
+//
+//  Created by Antonius F Aulia on 09/06/20.
+//  Copyright © 2020 Antonius F Aulia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
